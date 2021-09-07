333cd7413cfbcebbf5ed6d95f319553fcff0b96f
Felipe Moraes <felipemmarcello@gmail.com>
2021-09-07 00:47:33 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
+
+
 
 function App() {
   return (
@@ -8,7 +12,27 @@ function App() {
       <NavBar />
 
       <div className="container">
-        <h1 className="text-primary">Olá pessoal</h1>
+        <h1 className="text-primary py-2">Dashboard de Vendas!</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+
+            <BarChart/>
+
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
+
+            <DonutChart/>
+            
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary"> Todas as vendas</h2>
+        </div>
 
         <DataTable/>
 
---
+++ frontend/src/components/BarChart/index.tsx
@@ -0,0 +1,36 @@
+import Chart from 'react-apexcharts';
+
+const BarChart = () => {
+
+    const options = {
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+    };
+    
+    const mockData = {
+        labels: {
+            categories: ['Barry Allen', 'José', 'Alfredo', 'Abigail']
+        },
+        series: [
+            {
+                name: "% Sucesso",
+                data: [43.6, 67.1, 67.7, 45.6]                   
+            }
+        ]
+    };
+
+
+    return (
+        <Chart 
+            options={{...options, xaxis: mockData.labels}}
+            series= {mockData.series}
+            type="bar"
+            height="240"
+        />
+    );
+}
+
+export default BarChart;
