a2fe7a5d55bdf3d029ae6e7b74033a72f8b1e754
Felipe Moraes <felipemmarcello@gmail.com>
2021-09-11 16:53:11 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,18 +2,24 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className="container">
+        <><div className="container">
             <div className="jumbotron">
                 <h1 className="display-4">DSVendas</h1>
                 <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
                 <hr />
                 <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
-                
+
                 <Link className="btn btn-primary btn-lg" to="/dashboard">
                     Acessar dashboard
                 </Link>
             </div>
-        </div>
+        </div><footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/felipemmarcello" target="_blank" rel="noreferrer">Felipe Moraes</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer></>
     );
 }
 
