4407415ff3dbb62a188f62fa05dfd0dabde3671b
Felipe Moraes <felipemmarcello@gmail.com>
2021-09-11 12:51:31 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Página home</h1>
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-4">DSVendas</h1>
+                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                <hr />
+                <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                
+                <Link className="btn btn-primary btn-lg" to="/dashboard">
+                    Acessar dashboard
+                </Link>
+            </div>
         </div>
     );
 }
